7b8a546c5686ce322481688b1c255a1cc82d4ef0
ThCompiler <48956541+ThCompiler@users.noreply.github.com>
2019-07-22 12:56:11 +0500

    Update Solve-Quadr-Equation.cpp

--- Solve-Quadr-Equation.cpp
+++ Solve-Quadr-Equation.cpp
@@ -1,3 +1,4 @@
+//! @file
 #include <iostream>
 #include <assert.h>
 #include <math.h>
@@ -5,59 +6,15 @@
 using std::cin;
 using std::cout;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef unsigned int ui;
-
 namespace polynom
 {
-	const int cod_inf_num_of_sol = -1; ///< константа для обозначения бесконечного числа решений уравнения
+	const int CodeInfinityNumbersOfSolutions = -1; ///< константа для обозначения бесконечного числа решений уравнения
+	const double inaccuracy = 1e-9;
 
-	/*!
-		Определяет является ли введённое число нулём
-	*/
-	bool isZero(double p)
-	{
-		return abs(p) < 1e-9;
-	}
+	bool isZero(double p);
 
-	/*!
-		Функция, которая решает квадратное уравнение
-		\author TheCompiler
-		\date 17.07.2019
-		\param a,b,c коэфиценты квадратного уравнения
-		\param[out] x1,x2 корни уравнения
-		\return число корней
-	*/
-	int SolvQuadrEquat(double a, double b, double c, double* x1, double* x2)
-	{
-		assert(x1 != nullptr);
-		assert(x2 != nullptr);
-
-		if (isZero(a))
-		{
-			if (isZero(b))
-			{
-				if (isZero(c))
-				{
-					return cod_inf_num_of_sol;
-				}
-				return 0;
-			}
-			*x1 = -c / b;
-			*x2 = *x1;
-			return 1;
-		}
-		const double discr = b * b - 4 * a*c;  ///< Дискриминант
-		if (discr < 0)
-			return 0;
-		*x1 = sqrt(discr);
-		*x2 = (-b + *x1) / (2 * a);
-		*x1 = (-b - *x1) / (2 * a);
-		return 2;
-	}
+	int SolveQuadraticEquation(double a, double b, double c, double* x1, double* x2);
 }
-
 int main()
 {
 	double a = NAN, b = NAN, c = NAN;
@@ -69,30 +26,77 @@ int main()
 	cin >> b;
 	cout << "# Please, write third coef of equation:\nc = ";
 	cin >> c;
-	
-	int nm_of_sol = polynom::SolvQuadrEquat(a, b, c, &x1, &x2);
 
-	switch (nm_of_sol)
+	int nm_of_solution = polynom::SolveQuadraticEquation(a, b, c, &x1, &x2);
+
+	switch (nm_of_solution)
 	{
 	case 0:
 		cout << "# Equation doesn't have any solutions\n";
 		break;
 	case 1:
-		cout << "# Equation has one solution\n" 
-				<< "x = " << x1 << "\n";
+		cout << "# Equation has one solution\n"
+			<< "x = " << x1 << "\n";
 		break;
 	case 2:
-		cout << "# Equation has two solutions\n" 
-				<< "x1 = " << x1 << "\n"
-				<< "x2 = " << x2 << "\n";
+		cout << "# Equation has two solutions\n"
+			<< "x1 = " << x1 << "\n"
+			<< "x2 = " << x2 << "\n";
 		break;
-	case polynom::cod_inf_num_of_sol:
+	case polynom::CodeInfinityNumbersOfSolutions:
 		cout << "# Equation has infinity numbers of solutions\n";
 		break;
 	default:
 		cout << "# It's not good. You got unwaiting numbers of solutions.\n"
-				<< "Please, write on this email:cadabra@email.net.\n";
+			<< "Please, write on this email:cadabra@email.net.\n";
 		break;
 	}
 	return 0;
+}
+
+namespace polynom
+{
+	/*!
+		Определяет является ли введённое число нулём
+	*/
+	bool isZero(double p)
+	{
+		return abs(p) < inaccuracy;
+	}
+
+	/*!
+		Функция, которая решает квадратное уравнение
+		\author TheCompiler
+		\date 17.07.2019
+		\param a,b,c коэфиценты квадратного уравнения
+		\param[out] x1,x2 корни уравнения
+		\return число корней
+	*/
+	int SolveQuadraticEquation(double a, double b, double c, double* x1, double* x2)
+	{
+		assert(x1 != nullptr);
+		assert(x2 != nullptr);
+
+		if (isZero(a))
+		{
+			if (isZero(b))
+			{
+				if (isZero(c))
+				{
+					return CodeInfinityNumbersOfSolutions;
+				}
+				return 0;
+			}
+			*x1 = -c / b;
+			*x2 = *x1;
+			return 1;
+		}
+		const double discr = b * b - 4 * a * c;  ///< Дискриминант
+		if (discr < 0)
+			return 0;
+		*x1 = sqrt(discr);
+		*x2 = (-b + *x1) / (2 * a);
+		*x1 = (-b - *x1) / (2 * a);
+		return 2;
+	}
 }
